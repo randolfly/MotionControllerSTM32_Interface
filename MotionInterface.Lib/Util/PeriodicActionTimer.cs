ffb1c7a082614cd3bbf4b3f30a5f89bc6c708524
Randolf <1665718426@qq.com>
2024-06-20 15:19:56 +0800

    add timer util

---
+++ MotionInterface.Lib/Util/PeriodicActionTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace MotionInterface.Lib.Util;
+
+public class PeriodicActionTimer
+{
+    private Timer? _timer;
+    private int TimeInterval { get; set; } = 5000;
+    private Action TimerAction { get; set; }
+
+    public PeriodicActionTimer(Action timerAction, int timeInterval=5000)
+    {
+        TimerAction = timerAction;
+        TimeInterval = timeInterval;
+    }
+    
+    public void StartTimer()
+    {
+        _timer = new Timer(TimeInterval);
+        // Hook up the Elapsed event for the timer. 
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+    }
+
+    public void StopTimer()
+    {
+        _timer?.Stop();
+    }
+
+    private void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        // Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
+        TimerAction();
+    }
+    
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+
+}
