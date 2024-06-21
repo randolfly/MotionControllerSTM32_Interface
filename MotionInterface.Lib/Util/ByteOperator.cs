196bcf0030193c2f6ca0a42180f6a70768628e7c
Randolf <1665718426@qq.com>
2024-06-21 09:29:45 +0800

    update StringAndByteArrayConvesionTest

--- MotionInterface.Lib/Util/ByteOperator.cs
+++ MotionInterface.Lib/Util/ByteOperator.cs
@@ -3,6 +3,8 @@ using System.Text;
 
 namespace MotionInterface.Lib.Util;
 using System.Buffers.Binary;
+using System.Runtime.Intrinsics.X86;
+
 public static class ByteOperator
 {
     public static void ExtractTypeToBytes(this uint data, ref byte[] dataDest, uint offset = 0)
@@ -24,7 +26,7 @@ public static class ByteOperator
         dataDest[0 + offset] = (byte)(data & 0xFF);
     }
 
-    public static byte CalculateChecksum(byte[] data, int startIndex, int length,  byte init = 0)
+    public static byte CalculateChecksum(byte[] data, int startIndex, int length, byte init = 0)
     {
         var sum = init;
         for (var i = 0; i < length; i++)
@@ -39,19 +41,19 @@ public static class ByteOperator
     {
         return BinaryPrimitives.ReverseEndianness(data);
     }
-    
+
     public static ushort ReverseEndianness(this ushort data)
     {
         return BinaryPrimitives.ReverseEndianness(data);
     }
-    
+
     public static bool SequenceCompare(byte[]? x, byte[]? y)
     {
         if (ReferenceEquals(x, y)) return true;
         if (x is null || y is null) return false;
         return x.Length == y.Length && x.SequenceEqual(y);
     }
-    
+
     public static string ToHexString(this byte[] data)
     {
         var sb = new StringBuilder();
@@ -72,7 +74,7 @@ public static class ByteOperator
         }
         return floatArray;
     }
-    
+
     public static byte[] ToByteArray(this float[] data)
     {
         var byteArray = new byte[data.Length * 4];
@@ -82,4 +84,27 @@ public static class ByteOperator
         }
         return byteArray;
     }
+
+    public static string ToFloatString(this byte[] data, char seperator = ',')
+    {
+        var floatArray = data.ToFloatArray();
+        return floatArray.ToFloatString(seperator);
+    }
+
+    public static byte[] ToByteArray(this string rawCommand, char seperator = ',')
+    {
+        var floatArrary = ToFloatArray(rawCommand, seperator);
+        return floatArrary.ToByteArray();
+    }
+
+    public static string ToFloatString(this float[] data, char seperator = ',') {
+        return string.Join(seperator, data);
+    }
+
+    public static float[] ToFloatArray(this string rawCommand, char seperator = ',') {
+        var stringArray = rawCommand.Trim().Split(seperator);
+        var floatArray = stringArray.Select(float.Parse).ToArray();
+        return floatArray;
+    }
+
 }
--- MotionInterface.Test/ByteOperatorTest.cs
+++ MotionInterface.Test/ByteOperatorTest.cs
@@ -27,4 +27,25 @@ public class ByteOperatorTest
             Assert.Equal(floatArray[i], newFloatArray[i]);
         }
     }
+
+    [Fact]
+    public void StringAndByteArrayConvesionTest() {
+        // the latter test can be carried with random float array
+        var floatArray = new float[]
+        {
+            1.0f,1e-8f,0.1f,0.001f, 1.0f,0.0f,22222.29f,1990129012121.4f,211212e10f
+        };
+        var validateFloatString = string.Join(',',
+            floatArray.Select(f => f.ToString()).ToArray());
+        _testOutputHelper.WriteLine(validateFloatString);
+        var testFloatString = floatArray.ToFloatString();
+        Assert.Equal(validateFloatString, testFloatString);
+
+        // convert back
+        var testFloatArray = testFloatString.ToFloatArray();
+        for(var i=0; i<floatArray.Length; i++)
+        {
+            Assert.Equal(testFloatArray[i], floatArray[i]);
+        }
+    }
 }
